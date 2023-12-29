b1e7433e78159072e58d7a7539c653c341aef16a
Collin Hicks <hickscw2014@gmail.com>
2023-12-29 11:18:03 -0500

    file reading working

--- src/day_1/sample_parse.rs
+++ src/day_1/sample_parse.rs
@@ -1,23 +1,32 @@
 //import necesssary libraries here
 use std::{path::Path, error, mem::replace, io::{self, BufRead}, fs::File};
-use serde::{Deserialize, Serialize};
-use regex::Regex;
-
+use serde::Deserialize;
 
 // Define the struct for the object expected for the json file
-//  #[derive(Serialize, Deserialize, Debug)]
-struct Stock {
-    name: String,
-    symbol: String,
-    price: f64,
-    total_valuation:f64
+#[derive(Debug, serde::Deserialize)]
+pub struct Stock {
+    pub name: String,
+    pub symbol: String,
+    pub price: f64,
+    pub total_valuation:f64
 }
 
-pub fn parse_to_struct(){
-    
+pub fn parse_to_struct() -> Vec<Stock>{
+    let json_file = File::open("sample.json").expect("Could not open json file");
+    println!("{:?}", json_file);
+    let stocks:Vec<Stock> = serde_json::from_reader(json_file).expect("error reading json file");
+    println!("{:?}", stocks);
+    return stocks;
 }
 
-pub fn parse_to_json(){
-
+pub fn transform_and_write_to_json(mut stocks:Vec<Stock>){
+// let json_file = File::open("sample.json").expect("Could not open json file");
+// let mut new_stocks:Vec<Stock> = serde_json::from_reader(json_file).expect("error reading json file");
+for stock in &mut stocks{
+    let updated_sym = stock.symbol.to_ascii_uppercase();
+    stock.symbol = updated_sym;
+    println!("{}", stock.symbol)
+}
+println!("{:?}", stocks);
 
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::{env, path::Path};
 
 mod day_1;
 fn main() -> std::io::Result<()> {
-    day_1::sample_parse::parse_to_json();
+    let json_to_struct_stocks = day_1::sample_parse::parse_to_struct();
+    let stuct_to_json_file = day_1::sample_parse::transform_and_write_to_json(json_to_struct_stocks);
     Ok(())
     
 }
