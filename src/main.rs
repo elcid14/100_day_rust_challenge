a5ebff43c3d31dd327f212607250e8fed1723f0e
Collin Hicks <hickscw2014@gmail.com>
2024-01-01 12:43:35 -0500

    day 2 challenge complete

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use dotenv::dotenv;
 
 mod day_1;
+mod day_2;
 fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let json_to_struct_stocks = day_1::sample_parse::parse_to_struct(std::env::var("DAY_1_FILE_NAME").expect("Could not load env var"));
-    day_1::sample_parse::transform_and_write_to_json(json_to_struct_stocks).expect("Could not parse and write back to JSON file");
+    //day_1 code
+    
+    // let json_to_struct_stocks = day_1::sample_parse::parse_to_struct(std::env::var("DAY_1_FILE_NAME").expect("Could not load env var"));
+    // day_1::sample_parse::transform_and_write_to_json(json_to_struct_stocks).expect("Could not parse and write back to JSON file");
+    // Ok(())
+
+    //day_2 code
+    match day_2::reqwest_demo::get_reqwest(){
+        Ok(_) => println!("Success!"),
+        Err(err) => eprintln!("Error: {:?}", err)
+    }
+
     Ok(())
     
 }
--- target/debug/deps/one_hundred_days_challenge.d
+++ target/debug/deps/one_hundred_days_challenge.d
@@ -1,7 +1,9 @@
-C:\Users\hicks\Rust\portfolio\one-hundred-days-challenge\target\debug\deps\one_hundred_days_challenge.exe: src\main.rs src\day_1.rs src\day_1\sample_parse.rs
+C:\Users\hicks\Rust\portfolio\one-hundred-days-challenge\target\debug\deps\one_hundred_days_challenge.exe: src\main.rs src\day_1.rs src\day_1\sample_parse.rs src\day_2.rs src\day_2\reqwest_demo.rs
 
-C:\Users\hicks\Rust\portfolio\one-hundred-days-challenge\target\debug\deps\one_hundred_days_challenge.d: src\main.rs src\day_1.rs src\day_1\sample_parse.rs
+C:\Users\hicks\Rust\portfolio\one-hundred-days-challenge\target\debug\deps\one_hundred_days_challenge.d: src\main.rs src\day_1.rs src\day_1\sample_parse.rs src\day_2.rs src\day_2\reqwest_demo.rs
 
 src\main.rs:
 src\day_1.rs:
 src\day_1\sample_parse.rs:
+src\day_2.rs:
+src\day_2\reqwest_demo.rs:
